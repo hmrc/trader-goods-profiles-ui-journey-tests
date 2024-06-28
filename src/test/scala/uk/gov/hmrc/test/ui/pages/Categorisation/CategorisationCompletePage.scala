8e2b56068c51528527afc1f34f655046bfe8294a
mohsinkits <82995296+mohsinkits@users.noreply.github.com>
2024-06-28 18:30:31 +0100

    TGP-1515 - Updated the failed tests

--- src/test/scala/uk/gov/hmrc/test/ui/pages/Categorisation/CategorisationCompletePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/Categorisation/CategorisationCompletePage.scala
@@ -24,12 +24,12 @@ object CategorisationCompletePage extends BasePage {
 
   def title: String = "Categorisation complete"
   def h1: String    = "Categorisation complete"
-  def url: String   = "/update/b2fa315b-2d31-4629-90fc-a7b1a5119873/categorisation-result"
+  def url: String   = "/update/b2fa315b-2d31-4629-90fc-a7b1a5119873/categorisation-result/"
 
-  def assertPage(): this.type = {
+  def assertPage(Cat: String): this.type = {
     verifyTitle(title)
     verifyHeader(h1)
-    validateUrl(url)
+    validateUrl(url + Cat)
     this
   }
 
