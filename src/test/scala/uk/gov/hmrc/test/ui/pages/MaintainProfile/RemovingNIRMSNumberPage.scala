20110efbff076b5a22d3110b942076d881b36b5b
HardingM <37076550+HardingM@users.noreply.github.com>
2024-07-15 14:50:56 +0100

    TGP-1803: Maintain Profile journey url fix

--- src/test/scala/uk/gov/hmrc/test/ui/pages/MaintainProfile/RemovingNIRMSNumberPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/MaintainProfile/RemovingNIRMSNumberPage.scala
@@ -6,6 +6,6 @@ object RemovingNIRMSNumberPage extends Page {
 
   override def title(args: String*): String = "hasNirmsChange" // should be "Removing NIRMS details"
   override def h1(args: String*): String    = "Removing NIRMS details"
-  override def url(args: String*): String   = "/update-profile/removing-niphls-number"
+  override def url(args: String*): String   = "/update-profile/removing-nirms-number"
 
 }
