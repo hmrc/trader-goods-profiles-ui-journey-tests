688f2ffc4b4c02c4493c5e090bb4de1af296d68a
HardingM <37076550+HardingM@users.noreply.github.com>
2024-07-15 13:35:46 +0100

    TGP-1803: Maintain Profile journey remove commented code

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/MaintainProfile/ProfileDetailsStepDef.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/MaintainProfile/ProfileDetailsStepDef.scala
@@ -10,48 +10,23 @@ class ProfileDetailsStepDef extends BaseStepDef {
       .assertPage()
   }
 
-  Then("""^(?:My )?'(.*)' has the value (.*)$""") { (key1: String, key2: String) =>
-    key1 match {
+  Then("""^(?:My )?'(.*)' has the value (.*)$""") { (key: String, value: String) =>
+    key match {
       case "UKIMS Number" =>
         ProfileDetailsPage
-          .verifyInput("ukimsNumber", key2)
+          .verifyInput("ukimsNumber", value)
       case "NIPHL Question" =>
         ProfileDetailsPage
-          .verifyInput("niphlQuestion", key2)
+          .verifyInput("niphlQuestion", value)
       case "NIRMS Question" =>
         ProfileDetailsPage
-          .verifyInput("nirmsQuestion", key2)
+          .verifyInput("nirmsQuestion", value)
       case "NIPHL Number" =>
         ProfileDetailsPage
-          .verifyInput("niphlNumber", key2)
+          .verifyInput("niphlNumber", value)
       case "NIRMS Number" =>
         ProfileDetailsPage
-          .verifyInput("nirmsNumber", key2)
+          .verifyInput("nirmsNumber", value)
     }
   }
-
-//  Then("""^(?:My )?'UKIMS Number' has the value (.*)$""") { (key: String) =>
-//    ProfileDetailsPage
-//      .verifyInput("ukimsNumber", key)
-//  }
-//
-//  Then("""^(?:My )?'NIPHL Question' has the value (.*)$""") { (key: String) =>
-//    ProfileDetailsPage
-//      .verifyInput("niphlQuestion", key)
-//  }
-//
-//  Then("""^(?:My )?'NIRMS Question' has the value (.*)$""") { (key: String) =>
-//    ProfileDetailsPage
-//      .verifyInput("nirmsQuestion", key)
-//  }
-//
-//  Then("""^(?:My )?'NIPHL Number' has the value (.*)$""") { (key: String) =>
-//    ProfileDetailsPage
-//      .verifyInput("niphlNumber", key)
-//  }
-//
-//  Then("""^(?:My )?'NIRMS Number' has the value (.*)$""") { (key: String) =>
-//    ProfileDetailsPage
-//      .verifyInput("nirmsNumber", key)
-//  }
 }
