4692731c2c1cfd791e4a7731f620347f6465ef36
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-07-22 15:09:16 +0100

    Add mongo/mongosh checking

--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,23 @@ if [ -z "$BROWSER_TYPE" ]; then
     echo ""
 fi
 
-mongosh --eval "load('mongo-db-setup.js')"
+is_mongo_cmd_available() {
+  command -v mongo &> /dev/null;
+}
+
+is_mongosh_cmd_available() {
+  command -v mongosh &> /dev/null;
+}
+
+if is_mongosh_cmd_available; then
+    echo "Using mongosh";
+    mongosh --eval "load('mongo-db-setup.js')";
+elif is_mongo_cmd_available; then
+    echo "Using mongo";
+    mongo --eval "load('mongo-db-setup.js')";
+else
+    echo "No mongo/mongosh available";
+    exit 1;
+fi
 
 sbt -Dbrowser="${BROWSER_TYPE:=$DEFAULT_BROWSER}" -Denvironment="${ENV:=local}" -Dbrowser.option.headless=false -Daccessibility.assessment="false" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.TestRunner" testReport
