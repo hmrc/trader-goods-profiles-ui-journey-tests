9c809a7a309f6d7e5584ecda903f369dce325bb5
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-06-27 13:41:16 +0100

    Revert "Merge pull request #156 from hmrc/TGP-3503-Goods-record-pagetitle-updated"
    This reverts commit 5f30c81c1d7ad2e91f83222058e3b3701b230de7, reversing
    changes made to b5108d59dfe9ef7d454eff622a5f8ba5bc8ad772.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.46.0",
+    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.45.0",
     "org.scalatest"       %% "scalatest"              % "3.2.19",
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8",
     "io.cucumber"         %% "cucumber-scala"         % "8.26.1",
